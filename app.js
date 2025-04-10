2aed2bd9c0f57f300b2430e5bf629c3b2bad6efa
drwlodarczyk <dawid.r.wlodarczyk@gmail.com>
2025-04-10 18:56:55 +0200

    setup

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
-import express from 'express';
-import path from 'path';
+const express = require('express');
+const path = require('path');
 const fs = require('fs');
-import helmet from "helmet";
 
 const app = express();
+const PORT = process.env.PORT || 3000;
+const postsPath = path.join(__dirname, 'data/posts.json');
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
+// Ustawienie EJS i ścieżek
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-app.get('/', (req, res) => {
+// Middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
-})
+// Trasa główna (renderuje posty)
+app.get('/', (req, res) => {
+    const posts = JSON.parse(fs.readFileSync(postsPath));
+    res.render('index', { posts });
+});
